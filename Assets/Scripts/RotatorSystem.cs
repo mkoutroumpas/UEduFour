5e47338bce31821c27a8a95b06b814124cbc0bd0
mkoutroumpas <mkoutroumpas@gmail.com>
2022-09-18 13:42:20 +0200

    Construct a sample Cube entity inside RotatorSystem and add it to the Scene.

--- Assets/Scripts/RotatorSystem.cs
+++ Assets/Scripts/RotatorSystem.cs
@@ -1,5 +1,6 @@
 ﻿using Unity.Entities;
 using Unity.Jobs;
+using UnityEngine;
 
 public class RotatorSystem : JobComponentSystem
 {
@@ -11,6 +12,14 @@ public class RotatorSystem : JobComponentSystem
     protected override void OnCreate()
     {
         base.OnCreate();
+
+        GameObject cube = Resources.Load("Cube", typeof(GameObject)) as GameObject;
+
+        GameObjectConversionSettings settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
+
+        Entity cubeEntity = GameObjectConversionUtility.ConvertGameObjectHierarchy(cube, settings);
+
+        Entity cubeEntityInstance = EntityManager.Instantiate(cubeEntity);
 
     }
 }
