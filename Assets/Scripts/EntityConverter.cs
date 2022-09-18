27c88f9e58aad0c928f61c3f4e452f70b735a890
mkoutroumpas <mkoutroumpas@gmail.com>
2022-09-18 13:33:56 +0200

    1. Add ECS scaffolding, 2. Add EntityConverter script to Cube prefab.

---
+++ Assets/Scripts/EntityConverter.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class EntityConverter : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager entityManager, GameObjectConversionSystem gameObjectConversionSystem)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/RotatorSystem.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+
+public class RotatorSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        return default;
+    }
+
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+
+    }
+}
