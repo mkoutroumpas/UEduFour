658b0a787d6b2dc8e126235a15613a3c7bb86ad5
mkoutroumpas <mkoutroumpas@gmail.com>
2022-09-24 21:30:21 +0200

    Switch to IJobChunk implementation.

--- Assets/Scripts/RotatorSystem.cs
+++ Assets/Scripts/RotatorSystem.cs
@@ -3,28 +3,54 @@ using Unity.Jobs;
 using UnityEngine;
 using Unity.Mathematics;
 using Unity.Transforms;
+using Unity.Collections;
+using Unity.Burst;
 
 public class RotatorSystem : SystemBase
 {
+    private EntityQuery entityQuery;
+
+    [BurstCompile]
+    struct RotatorJob : IJobChunk
+    {
+        public float DeltaTime;
+        public ArchetypeChunkComponentType<Rotation> RotationArchetypeChunkComponentType;
+        [ReadOnly] public ArchetypeChunkComponentType<Rotator> RotatorArchetypeChunkComponentType;
+        public void Execute(ArchetypeChunk chunk, int chunkIndex, int firstEntityIndex)
+        {
+            var chunkRotations = chunk.GetNativeArray(RotationArchetypeChunkComponentType);
+            var chunkRotators = chunk.GetNativeArray(RotatorArchetypeChunkComponentType);
+
+            for (var i = 0; i < chunk.Count; i++)
+            {
+                Rotation rotation = chunkRotations[i];
+                Rotator rotator = chunkRotators[i];
+
+                chunkRotations[i] = new Rotation
+                {
+                    Value = math.mul(math.normalize(rotation.Value),
+                        quaternion.AxisAngle(math.up(), rotator.RotationSpeed * DeltaTime))
+                };
+            }
+        }
+    }
     protected override void OnUpdate()
     {
-        float deltaTime = Time.DeltaTime;
+        RotatorJob rotatorJob = new RotatorJob()
+        {
+            RotationArchetypeChunkComponentType = GetArchetypeChunkComponentType<Rotation>(false),
+            RotatorArchetypeChunkComponentType = GetArchetypeChunkComponentType<Rotator>(true),
+            DeltaTime = Time.DeltaTime
+        };
 
-        Entities
-            .WithBurst()
-            .WithName("CubeRotatorSystem")
-            .ForEach((ref Rotation rotation, in Rotator rotator) => 
-            {
-                rotation.Value = math.mul(
-                    math.normalize(rotation.Value),
-                    quaternion.AxisAngle(math.up(), rotator.RotationSpeed * deltaTime));
-            })
-            .ScheduleParallel();
+        this.Dependency =  rotatorJob.ScheduleParallel(entityQuery, this.Dependency);
     }
 
     protected override void OnCreate()
     {
         base.OnCreate();
+
+        entityQuery = GetEntityQuery(ComponentType.ReadOnly<Rotator>());
 
         GameObject cube = Resources.Load("Cube", typeof(GameObject)) as GameObject;
 
